0dfcbd0c1ab1a7bfa45f872234c5dff25adb6f0c
Rahul Chauhan <rahuchau@rahuchau-macos.corp.adobe.com>
2017-07-28 17:25:29 +0530

    adding a test client

--- gs-rest-service/initial/src/main/java/hello/Greeting.java
+++ gs-rest-service/initial/src/main/java/hello/Greeting.java
@@ -5,6 +5,12 @@ public class Greeting {
     private final long id;
     private final String content;
 
+    public Greeting()
+    {
+        id=0;
+        content=null;
+    }
+
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
@@ -16,5 +22,13 @@ public class Greeting {
 
     public String getContent() {
         return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
